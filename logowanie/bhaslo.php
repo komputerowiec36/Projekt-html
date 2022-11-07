9bfa05fad92bb4fac32288973c1b23562c96e0b0
komputerowiec36 <49672692+komputerowiec36@users.noreply.github.com>
2022-11-07 18:17:51 +0100

    Add files via upload

---
+++ logowanie/bhaslo.php
@@ -0,0 +1,7 @@
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<body>
+<body bgcolor="#FF6633">
+<?php
+echo "Błędne hasło zaloguj się ponownie";
+echo "<br><br><a href=\"../logowanie.html?a=wróć&amp\">Wróć</a>";
+?>
---
+++ logowanie/kod.php
@@ -0,0 +1,7 @@
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<body>
+<body bgcolor="#FF6633">
+<?php
+echo "Kod zklada się z 8 liczb!";
+echo "<br><br><a href=\"../logowanie.html?a=wróć&amp\">Wróć</a>";	
+?>
